58e14bacd223c4547e66ebba185a3f47985acae3
Filippo Vitale <fvitale@gmail.com>
2015-09-04 10:55:22 -0700

    T10 add score number increasing by clicking the button

--- t10-purescript-tutorial/src/Main.purs
+++ t10-purescript-tutorial/src/Main.purs
@@ -7,17 +7,27 @@ import Control.Monad.Aff.Console
 import Halogen
 import Halogen.Util
 import qualified Halogen.HTML as H
+import qualified Halogen.HTML.Events as E
 
 data Input a = Click a
 
 render :: forall p. Render Int Input p
-render _ = H.div_ [ H.text "Hello world" ]
+render n = H.div_ [
+                    H.text $ "Score: " ++ show n,
+                    H.button [ E.onClick $ E.input_ Click ]
+                             [ H.text "Click me!" ]
+                  ]
 
 eval :: forall f. Eval Input Int Input f
-eval (Click a) = pure a
+-- eval has state Monad built in
+eval (Click a) = do
+  modify (+ 1)
+  pure a
 
 ui :: forall f p. Component Int Input f p
 ui = component render eval
 
 main = A.launchAff do
+  app <- runUI ui 0
+  appendToBody app.node
   log "Hello sailor!"
